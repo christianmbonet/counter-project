c37bdaa82dfb2f014c32a94ebf3423947658e42e
christianmbonet <christianm.bonet@gmail.com>
2021-05-07 09:48:47 -0400

    set up counter using useState hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React, { useState } from "react";
+import './../components/index.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+      setCounter(prevCounter => prevCounter + 1)
+    }
+
+    const decrement = () => {
+      setCounter(prevCounter => prevCounter - 1)
+    }
+
+  return (
+    <div>
+      <div className='container'>
+        <button className='subtractButton' onClick={decrement}>-</button>
+        <div className='counter'>{counter}</div>
+        <button className='addButton' onClick={increment}>+</button>
+      </div>
+    </div>
+  )
 }
